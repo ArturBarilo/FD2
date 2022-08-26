50809bab251dcbde069bc19dc1c257474a88c845
Artur_Barilo <bariloartur@gmail.com>
2022-08-26 14:22:23 +0300

    lesson 1

---
+++ lesson 01/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// alert('Hello!!');
+
+// console.log('Hello');
+// console.log('Hello');
+
+// let a = 'notebook';
+// console.log(a);
+
+// a = 'apple';
+// console.log(a);
+
+// const bTrue = true;
+// const bFalse = false;
+
+// const arr = [1, 2, 3];
+
+// const user = {
+//     name: "John",
+// };
+
+// console.log(arr);
+// console.log(user);
+
+// let newVar;
+
+// if (bTrue) {
+//     newVar = 'test';
+//     console.log('здесь true')
+// };
+
+// console.log(newVar)
+
+// if (a === 'apple') {
+//     console.log('a === apple is true')
+// } else {
+//     console.log('False')
+// };
+
+
+
+for (let index = 25;; index +=1 ) {
+    console.log('index = ' + index);
+
+    if (index % 17 === 0) {
+        console.log('Нашли index ' + index);
+        break;
+    }
+};
